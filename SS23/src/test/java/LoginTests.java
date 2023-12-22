d125f2d30dcc35b9ea9aa5637af10d69ddc5358b
Krossovkin <krossovkin4@gmail.com>
2023-12-22 16:16:13 +0200

    add LoginTest

---
+++ SS23/src/test/java/LoginTests.java
@@ -0,0 +1,26 @@
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginTests {
+@BeforeClass
+    public void preCondition(){
+
+}
+@Test
+public void loginSuccess(){
+
+}
+@Test
+public void loginWrongEmail(){
+
+}
+@Test
+public void loginWrongPassword(){
+
+}
+@AfterClass
+public void postConditions(){
+
+}
+}
